09bf6baee95a8eb8e83d599bcbc88ca6ea121901
Nima Askarian <88832088+nimaaskarian@users.noreply.github.com>
2022-03-25 17:11:02 +0430

    Delete build directory

--- build/js/ac.js
+++
@@ -1,8 +0,0 @@
-function autocompleteCallback(e) {
-  var evt = document.createEvent("Event");
-  evt.initEvent("autocomplete", true, true);
-  evt.ac = e;
-  document.dispatchEvent(evt);
-}
-
-var chrome = chrome || null;
